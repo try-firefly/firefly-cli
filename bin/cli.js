c27acdae713e6273b8ddc4cff14c3ee9c31eb8c0
Julia-Park <julia.ay.park@gmail.com>
2022-11-04 18:25:50 -0400

    Added Firefly layer publishing via sdk
    Co-authored-by: wyennie <wryennie@gmail.com>
    Co-authored-by: CarolinaDAvila <carolinaavila@protonmail.com>
    Co-authored-by: MarcusSinkinson <sinkinsonmarcus@gmail.com>

--- bin/cli.js
+++ bin/cli.js
@@ -1,76 +1,82 @@
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+const AWS = require("aws-sdk");
 const inquirer = require('inquirer');
 const { createYamlZip } = require('../src/helpers/yaml-generator');
 const { createFireflyLayer } = require('../src/helpers/layer-generator');
+const AdmZip = require("adm-zip");
+const iam = new AWS.IAM();
+let lambda;
+let otelConfigLayerArn;
+let fireflyLayerArn;
 
-const suppportRegions = [
-  "ap-northeast-1",
-  "ap-northeast-2",
-  "ap-south-1",
-  "ap-southeast-1",
-  "ap-southeast-2",
-  "ca-central-1",
-  "eu-central-1",
-  "eu-north-1",
-  "eu-west-1",
-  "eu-west-2",
-  "eu-west-3",
-  "sa-east-1",
-  "us-east-1",
-  "us-east-2",
-  "us-west-1",
-  "us-west-2",
-];
+const supportedRegions = {
+  'x86_64': [
+    "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-south-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ca-central-1",
+    "eu-central-1",
+    "eu-north-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "sa-east-1",
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2"
+  ],
+  'arm64': [
+    "ap-northeast-1",
+    "ap-south-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "eu-central-1",
+    "eu-west-1",
+    "eu-west-2",
+    "us-east-1",
+    "us-east-2",
+    "us-west-2"
+  ]
+}
 
 const arnLambdaInsights = {
-  "ap-northeast-1": "arn:aws:lambda:ap-northeast-1:580247275435:layer:LambdaInsightsExtension:32",
-  "ap-northeast-2": "arn:aws:lambda:ap-northeast-2:580247275435:layer:LambdaInsightsExtension:20",
-  "ap-south-1": "arn:aws:lambda:ap-south-1:580247275435:layer:LambdaInsightsExtension:21",
-  "ap-southeast-1": "arn:aws:lambda:ap-southeast-1:580247275435:layer:LambdaInsightsExtension:21",
-  "ap-southeast-2": "arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension:21",
-  "ca-central-1": "arn:aws:lambda:ca-central-1:580247275435:layer:LambdaInsightsExtension:20",
-  "eu-central-1": "arn:aws:lambda:eu-central-1:580247275435:layer:LambdaInsightsExtension:21",
-  "eu-north-1": "arn:aws:lambda:eu-north-1:580247275435:layer:LambdaInsightsExtension:20",
-  "eu-west-1": "arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:21",
-  "eu-west-2": "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:21",
-  "eu-west-3": "arn:aws:lambda:eu-west-3:580247275435:layer:LambdaInsightsExtension:20",
-  "sa-east-1": "arn:aws:lambda:sa-east-1:580247275435:layer:LambdaInsightsExtension:20",
-  "us-east-1": "arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension:21",
-  "us-east-2": "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:21",
-  "us-west-1": "arn:aws:lambda:us-west-1:580247275435:layer:LambdaInsightsExtension:20",
-  "us-west-2": "arn:aws:lambda:us-west-2:580247275435:layer:LambdaInsightsExtension:21",
+  'x86_64': {
+    "ap-northeast-1": "arn:aws:lambda:ap-northeast-1:580247275435:layer:LambdaInsightsExtension:32",
+    "ap-northeast-2": "arn:aws:lambda:ap-northeast-2:580247275435:layer:LambdaInsightsExtension:20",
+    "ap-south-1": "arn:aws:lambda:ap-south-1:580247275435:layer:LambdaInsightsExtension:21",
+    "ap-southeast-1": "arn:aws:lambda:ap-southeast-1:580247275435:layer:LambdaInsightsExtension:21",
+    "ap-southeast-2": "arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension:21",
+    "ca-central-1": "arn:aws:lambda:ca-central-1:580247275435:layer:LambdaInsightsExtension:20",
+    "eu-central-1": "arn:aws:lambda:eu-central-1:580247275435:layer:LambdaInsightsExtension:21",
+    "eu-north-1": "arn:aws:lambda:eu-north-1:580247275435:layer:LambdaInsightsExtension:20",
+    "eu-west-1": "arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:21",
+    "eu-west-2": "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:21",
+    "eu-west-3": "arn:aws:lambda:eu-west-3:580247275435:layer:LambdaInsightsExtension:20",
+    "sa-east-1": "arn:aws:lambda:sa-east-1:580247275435:layer:LambdaInsightsExtension:20",
+    "us-east-1": "arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension:21",
+    "us-east-2": "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:21",
+    "us-west-1": "arn:aws:lambda:us-west-1:580247275435:layer:LambdaInsightsExtension:20",
+    "us-west-2": "arn:aws:lambda:us-west-2:580247275435:layer:LambdaInsightsExtension:21",
+  },
+  'arm64': {
+    "us-east-1": "arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "us-east-2": "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "us-west-2": "arn:aws:lambda:us-west-2:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "ap-south-1": "arn:aws:lambda:ap-south-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "ap-southeast-1": "arn:aws:lambda:ap-southeast-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "ap-southeast-2": "arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "ap-northeast-1": "arn:aws:lambda:ap-northeast-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "eu-central-1": "arn:aws:lambda:eu-central-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "eu-west-1": "arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension-Arm64:2",
+    "eu-west-2": "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension-Arm64:2"
+  }
 }
 
-async function getFunctionList() {
-  let list;
-
-  try {
-    logger('Retrieving your lambda functions');
-    list = await exec("aws lambda list-functions");
-  } catch (e) {
-    console.log('Please make sure you have installed the AWS CLI');
-    console.log(`Error: ${e}`);
-  }
-
-  list = JSON.parse(list.stdout);
-  return extractFunctionData(list.Functions);
-}
-
-function extractFunctionData(functions) {
-  const functionData = [];
-
-  for (let i = 0; i < functions.length; i++) {
-    const f = functions[i];
-    const fObj = {};
-    fObj.name = f.FunctionName;
-    fObj.region = getRegion(f.FunctionArn);
-    fObj.runtime = f.Runtime;
-    fObj.role = getRole(f.Role);
-    functionData.push(fObj);
-  }
-
-  return functionData;
+async function getList() {
+  const data = await lambda.listFunctions().promise();
+  return extractFunctionData(data.Functions);
 }
 
 function getRole(arn) {
@@ -89,6 +95,55 @@ function completionLogger(str) {
   console.log('\u2714 ', str);
 }
 
+function extractFunctionData(functions) {
+  const functionData = [];
+
+  for (let i = 0; i < functions.length; i++) {
+    const f = functions[i];
+    const fObj = {};
+    fObj.name = f.FunctionName;
+    fObj.region = getRegion(f.FunctionArn);
+    fObj.runtime = f.Runtime;
+    fObj.role = getRole(f.Role);
+    fObj.roleArn = f.Role;
+    fObj.architecture = f.Architectures[0];
+    functionData.push(fObj);
+  }
+
+  return functionData;
+}
+
+async function getHttpsAddress() {
+  const question = {
+    type: 'input',
+    name: 'httpsAddress',
+    prefix: '',
+    message: 'Please provide an HTTPS address to send Telemetry data to (https://example.com):',
+    validate: httpsAddressValid
+  }
+
+  const answer = await inquirer.prompt(question);
+  return answer.httpsAddress;
+}
+
+async function httpsAddressValid(address) {
+  const pattern = new RegExp('^https://.*');
+  return pattern.test(address);
+}
+
+async function setRegion() {
+  const question = {
+    type: 'input',
+    name: 'region',
+    prefix: '',
+    message: 'Please provide your AWS region:',
+  }
+
+  const answer = await inquirer.prompt(question);
+  const region = answer.region;
+  lambda = new AWS.Lambda({region});
+}
+
 async function getFunctionsToInstrument(functionList) {
   const functionNames = functionList.map(f => f.name);
 
@@ -104,149 +159,101 @@ async function getFunctionsToInstrument(functionList) {
   return functionList.filter(f => answers.functions.includes(f.name));
 }
 
-async function getHttpsAddress() {
-  const question = {
-    type: 'input',
-    name: 'httpsAddress',
-    prefix: '',
-    message: 'Please provide an HTTPS address to send Telemetry data to (https://example.com):',
-    validate: httpAddressValid
+async function publishLayer(layerName, layerZipLocation) {
+  const zip = new AdmZip(layerZipLocation).toBuffer();
+
+  let params = {
+    Content: {
+      ZipFile: zip
+    },
+    LayerName: layerName,
   }
-
-  const answer = await inquirer.prompt(question);
-  return answer.httpsAddress;
+  
+  let result = await lambda.publishLayerVersion(params).promise();
+  return result.LayerVersionArn;
 }
 
-async function httpAddressValid(address) {
-  const pattern = new RegExp('^https://.*');
-  return pattern.test(address);
+async function publishOtelConfigLayer() {
+  logger('Publishing OpenTelemetry config layer');
+  otelConfigLayerArn = await publishLayer('otel-collector-config', './collector.zip');
+  completionLogger('OpenTelemetry config layer published');
 }
 
-function getOtelCollector(runtime) {
-  if (runtime.match(/.*node.*/gi)) {
+async function publishFireflyLayer() {
+  logger('Publishing Firefly layer');
+  fireflyLayerArn = await publishLayer('firefly-layer', './firefly-layer.zip');
+  completionLogger('Firefly layer published');
+}
+
+function getOtelCollector(runtime, architecture) {
+  if (runtime.match(/.*node.*/gi) && architecture === 'x86_64') {
     return "aws-otel-nodejs-amd64-ver-1-7-0:1";
-  } else if (runtime.match(/.*python.*/gi)) {
-    return "aws-otel-python-amd64-ver-1-13-0:1"
+  } else if (runtime.match(/.*node.*/gi) && architecture === 'arm64') {
+    return "aws-otel-nodejs-arm64-ver-1-7-0:1";
   } else {
     return null;
   }
 }
 
-async function publishConfigLayer() {
-  const cmd = "aws lambda publish-layer-version --layer-name otel-collector-config --zip-file fileb://collector.zip";
-  logger('Publishing configuration layer');
-  const result = await exec(cmd);
-  completionLogger('Configuration layer published');
-  const output = JSON.parse(result.stdout);
-  return output.LayerArn + `:${output.Version}`;
-}
-
-async function publishFireflyLayer() {
-  const cmd = "aws lambda publish-layer-version --layer-name firefly-lambda-layer --zip-file fileb://firefly-layer.zip";
-  logger('Publishing Firefly layer');
-  const result = await exec(cmd);
-  completionLogger('Firefly layer published');
-  const output = JSON.parse(result.stdout);
-  return output.LayerArn + `:${output.Version}`;
-}
-
-async function waitForAws(sec) {
-  return new Promise(resolve => setTimeout(resolve, sec * 1000));
-}
-
-async function addLayers(addLayersCmd) {
-  logger('Adding OpenTelemetry collector, Lambda Insights and Firefly layers');
-  await exec(addLayersCmd);
-  completionLogger('Layers added');
-  await waitForAws(2);
-}
-
-async function addTracingPolicy(addTracePolicyCmd) {
-  logger('Adding tracing policy');
-  await exec(addTracePolicyCmd);
-  completionLogger('Tracing policy added');
-  await waitForAws(2);
-}
-
-async function deactivateTracing(setTracingCmd) {
-  logger('Deactivating active tracing');
-  await exec(setTracingCmd);
-  completionLogger('Active tracing deactivated');
-  await waitForAws(2);
-}
-
-async function changeLambdaHandler(changeHandlerCmd) {
-  logger('Changing default Lambda handler to Firefly handler');
-  await exec(changeHandlerCmd);
-  completionLogger('Lambda handler changed');
-  await waitForAws(2);
-}
-
-async function addEnhancedMonitoringPolicy(addEnhancedMonitoringPolicyCmd) {
-  logger('Adding enhanced monitoring policy');
-  await exec(addEnhancedMonitoringPolicyCmd);
-  completionLogger('Enhanced monitoring policy added');
-  await waitForAws(2);
-}
-
-async function addEnvironmentVariables(addEnvVariablesCmd) {
-  logger('Adding environment variables');
-  await exec(addEnvVariablesCmd);
-  completionLogger('Environment variables added');
-}
-
 async function instrumentFunctions(functionsToInstrument) {
-  const otelConfigArn = await publishConfigLayer();
-  const fireflyArn = await publishFireflyLayer();
-
   for (const fObj of functionsToInstrument) {
-    console.log('');
     logger(`Instrumenting ${fObj.name}`);
-    const otel = getOtelCollector(fObj.runtime);
+    const otel = getOtelCollector(fObj.runtime, fObj.architecture);
 
     if (!otel) {
       logger(`${fObj.name} can't be instrumented, ${fObj.runtime} runtime not supported`);
       continue;
     }
-
-    if (!suppportRegions.includes(fObj.region)) {
+  
+    if (!supportedRegions[fObj.architecture].includes(fObj.region)) {
       logger(`${fObj.name} can't be instrumented, ${fObj.region} not supported`);
       continue;
     }
 
-    const configPath = fObj.runtime.match(/.*node.*/gi) ? "/opt/collector.yaml" : "/opt/otel-instrument";
-    const otelArn = `arn:aws:lambda:${fObj.region}:901920570463:layer:${otel}`;
-    const envVariables = `Variables={AWS_LAMBDA_EXEC_WRAPPER=/opt/otel-handler,OPENTELEMETRY_COLLECTOR_CONFIG_FILE=${configPath},OTEL_PROPAGATORS=tracecontext,OTEL_TRACES_SAMPLER=always_on}`;
-    const addLayersCmd = `aws lambda update-function-configuration --function-name ${fObj.name} --layers ${otelArn} ${otelConfigArn} ${fireflyArn} ${lambdaInsightsArn}`;
-    const lambdaInsightsArn = arnLambdaInsights[fObj.region];
-    //`arn:aws:lambda:${fObj.region}:580247275435:layer:LambdaInsightsExtension:21`
-    const addTracePolicyCmd = `aws iam attach-role-policy --role-name ${fObj.role} --policy-arn "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"`
-    const setTracingCmd = `aws lambda update-function-configuration --function-name ${fObj.name} --tracing-config Mode=PassThrough`;
-    const changeHandlerCmd = `aws lambda update-function-configuration --function-name ${fObj.name} --handler=/opt/nodejs/firefly-handler.handler`;
-    const addEnhancedMonitoringPolicyCmd = `aws iam attach-role-policy --role-name ${fObj.role} --policy-arn "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy"`
-    const addEnvVariablesCmd = `aws lambda update-function-configuration --function-name ${fObj.name} --environment "${envVariables}"`;
+    const params = {
+      FunctionName: fObj.name,
+      Environment: {
+        Variables: {
+          'AWS_LAMBDA_EXEC_WRAPPER': '/opt/otel-handler',
+          'OPENTELEMETRY_COLLECTOR_CONFIG_FILE': '/opt/collector.yaml',
+          'OTEL_PROPAGATORS': 'tracecontext',
+          'OTEL_TRACES_SAMPLER': 'always_on'
+        }
+      },
+      Handler: '/opt/nodejs/firefly-handler.handler',
+      Layers: [
+        otelConfigLayerArn,
+        fireflyLayerArn,
+        `arn:aws:lambda:${fObj.region}:901920570463:layer:${otel}`, // OTel layer
+        arnLambdaInsights[fObj.architecture][fObj.region] // Lambda Insights layer
+      ],
+      Role: fObj.roleArn,
+      TracingConfig: {
+        Mode: 'PassThrough'
+      }
+    };
 
-    try {
-      await addLayers(addLayersCmd);
-      // await addTracingPolicy(addTracePolicyCmd);
-      await changeLambdaHandler(changeHandlerCmd);
-      await deactivateTracing(setTracingCmd);
-      // await addEnhancedMonitoringPolicy(addEnhancedMonitoringPolicyCmd);
-      await addEnvironmentVariables(addEnvVariablesCmd);
-    } catch (e) {
-      console.log(e);
-    }
+    const iamParams = {
+      PolicyArn: "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy", 
+      RoleName: fObj.role
+    };
+  
+    await iam.attachRolePolicy(iamParams).promise();
+    await lambda.updateFunctionConfiguration(params).promise();
   }
 
-  completionLogger("Instrumentation complete");
+  completionLogger('Instrumentation complete');
 }
 
 async function main() {
+  await setRegion();
   const httpsAddress = await getHttpsAddress();
   await createYamlZip(httpsAddress);
   await createFireflyLayer();
-  const functionList = await getFunctionList();
+  const functionList = await getList();
   const functionsToInstrument = await getFunctionsToInstrument(functionList);
+  await publishOtelConfigLayer();
+  await publishFireflyLayer();
   await instrumentFunctions(functionsToInstrument);
 }
 
